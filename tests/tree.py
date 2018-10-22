1f798ee0cf72118661eb1c71e9b4c2bd2be81f71
qiaojushuang <747848783@qq.com>
2018-10-22 23:17:51 +0800

    tree test

---
+++ tests/tree.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# coding=utf-8
+
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+
+    def construct_tree(self, values=None):
+        """
+        :param values: list
+        :return:
+        """
+        if not values:
+            return None
+        self.root = TreeNode(values[0])
+        queue = deque([self.root])
+        leng = len(values)
+        nums = 1
+        while nums < leng:
+            node = queue.popleft()
+            if node:
+                node.left = TreeNode(values[nums]) if values[nums] else None
+                queue.append(node.left)
+                if nums + 1 < leng:
+                    node.right = TreeNode(values[nums + 1]) if values[nums + 1] else None
+                    queue.append(node.right)
+                    nums += 1
+                nums += 1
+
+    def bfs(self):
+        ret = []
+        queue = deque([self.root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                ret.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+        return ret
+
+    def pre_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            ret.append(head.val)
+            traversal(head.left)
+            traversal(head.right)
+
+        traversal(self.root)
+        return ret
+
+    def in_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            traversal(head.left)
+            ret.append(head.val)
+            traversal(head.right)
+
+        traversal(self.root)
+        return ret
+
+    def post_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            traversal(head.left)
+            traversal(head.right)
+            ret.append(head.val)
+
+        traversal(self.root)
+        return ret
+
+
+t = Tree()
+t.construct_tree([1, 2, None, 4, 3, None, 5])
+print t.bfs()
+print t.pre_traversal()
+print t.in_traversal()
+print t.post_traversal()
