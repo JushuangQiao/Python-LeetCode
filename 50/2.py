7501fec0adc8a2ddaf5c29dcc4bd477c0e04d914
qiaojushuang <747848783@qq.com>
2019-02-25 23:37:08 +0800

    2

--- 50/2.py
+++ 50/2.py
@@ -11,23 +11,16 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        a = b = 0
+        head = move = ListNode(0)
         carry = 0
-        while l1:
-            a += l1.val * 10 ** carry
-            carry += 1
-            l1 = l1.next
-        carry = 0
-        while l2:
-            b += l2.val * 10 ** carry
-            carry += 1
-            l2 = l2.next
-        ret = a + b
-        h = m = ListNode(0)
-        if not ret:
-            return h
-        while ret:
-            m.next = ListNode(ret % 10)
-            ret /= 10
-            m = m.next
-        return h.next
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            move.next = ListNode(carry % 10)
+            carry /= 10
+            move = move.next
+        return head.next
